5a5966896d2b8064b16df326be8b484d3b188255
juanweihou <1049090448@qq.com>
2026-09-09 00:16:36 +0800

    test(migration): publish extended 0011 checksum

--- internal/migration/admin_operation_safety_test.go
+++ internal/migration/admin_operation_safety_test.go
@@ -427,7 +427,7 @@ func TestMigrationSequencePreservesPublishedChecksums(t *testing.T) {
 		{"0008", "21289da334e7ef4425f697c659e6f45227e88c4d86ac4c099867dd6895f666f2"},
 		{"0009", "4dc818d93180bb6777d2ec6d8318e728fe76add4c736a178f19b808ca2afedf7"},
 		{"0010", "b6ddd5b7088f1617b9831186e08da622f7d06cbe985707ef9f5524ffe6057780"},
-		{"0011", "04fdec9c97d6f5fa4cde6f3ee93109bb5bb040a142928c24ba1766aae669a098"},
+		{"0011", "be6ea3beb18a64cf2a2e03b2730df5abbcc457900e9da6292eefee0dd15a2792"},
 	}
 	if len(want) != len(migrations) {
 		t.Fatalf("checksum list length = %d, migrations = %d", len(want), len(migrations))
